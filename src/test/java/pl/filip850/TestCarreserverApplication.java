faccab58f7ed30ef9da0e90c5f4d3821e39e2d14
filip850 <filski850@gmail.com>
2026-05-28 14:08:45 +0200

    chore(tests): Commented out TestContainers - will be not used for now

--- src/test/java/pl/filip850/TestCarreserverApplication.java
+++ src/test/java/pl/filip850/TestCarreserverApplication.java
@@ -1,11 +1,11 @@
-package pl.filip850;
-
-import org.springframework.boot.SpringApplication;
-
-public class TestCarreserverApplication {
-
-  public static void main(String[] args) {
-    SpringApplication.from(CarreserverApplication::main).with(TestcontainersConfiguration.class).run(args);
-  }
-
-}
+//package pl.filip850;
+//
+//import org.springframework.boot.SpringApplication;
+//
+//public class TestCarreserverApplication {
+//
+//  public static void main(String[] args) {
+//    SpringApplication.from(CarreserverApplication::main).with(TestcontainersConfiguration.class).run(args);
+//  }
+//
+//}
